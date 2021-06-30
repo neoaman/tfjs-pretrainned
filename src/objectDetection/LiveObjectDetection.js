19b61eea1ff8af308a6db85393006149cc3f52c4
neoaman <amanthe001@gmail.com>
2021-06-30 19:48:31 +0530

    Changed Facing model based on codepen

--- src/objectDetection/LiveObjectDetection.js
+++ src/objectDetection/LiveObjectDetection.js
@@ -4,26 +4,37 @@ import { useState } from "react";
 import { IconButton } from "@material-ui/core";
 import { IoCameraReverse } from "react-icons/all";
 
+const FACING_MODE_USER = "user";
+const FACING_MODE_ENVIRONMENT = "environment";
+
 const LiveObjectDetection = () => {
-  const [facingMode, setFacingmode] = useState("user");
+  const [facingMode, setFacingMode] = useState(FACING_MODE_USER);
+  const videoConstraints = {
+    facingMode: FACING_MODE_USER,
+  };
 
   return (
     <div>
       <IconButton
-        onClick={() =>
-          facingMode === "user"
-            ? setFacingmode("forward")
-            : facingMode === "forward"
-            ? setFacingmode("selfie")
-            : setFacingmode("user")
-        }
+        onClick={() => {
+          setFacingMode((prevState) =>
+            prevState === FACING_MODE_USER
+              ? FACING_MODE_ENVIRONMENT
+              : FACING_MODE_USER
+          );
+          console.log(facingMode);
+        }}
       >
         <IoCameraReverse></IoCameraReverse>
       </IconButton>
-      {/* <Webcam videoConstraints={{ facingMode: facingMode }}></Webcam> */}
       <Webcam
-        videoConstraints={{ facingMode: { exact: "environment" } }}
-      ></Webcam>
+        audio={false}
+        screenshotFormat="image/jpeg"
+        videoConstraints={{
+          ...videoConstraints,
+          facingMode,
+        }}
+      />
     </div>
   );
 };
